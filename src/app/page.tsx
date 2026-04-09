099f8ce31f3f2a84188d4c80e7ee2df6fb743566
Sxm-dev4 <sammm204@outlook.com>
2026-04-09 17:48:57 -0500

    chore(backend): limpiar estructura legacy y dejar backend api-only

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "@/components/home/Hero";
-
-export default function Home() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Hero />
-        </div>
-    );
-}
--- src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthContainer from "@/components/auth/AuthContainer";
-
-export default function RegisterPage() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <AuthContainer initialMode="register" />
-        </div>
-    );
-}
